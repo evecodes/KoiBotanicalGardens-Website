df33ca7339e8c1d1a45ddb6121580420189451cf
evecodes <99173892+evecodes@users.noreply.github.com>
2022-10-27 15:27:08 +0200

    More JS added
    More JS added.

--- main.js
+++ main.js
@@ -12,21 +12,86 @@ hamburgerMenu.addEventListener('click', e => {
 });
 
 linkHome.addEventListener('click', e => {
-    navToggle.classList.toggle('nav-hide');
-    hamburgerStripe.classList.toggle('stripe-active');
+    timeout = setTimeout(navHideDelay, 200);
 })
 
 linkGarden.addEventListener('click', e => {
-    navToggle.classList.toggle('nav-hide');
-    hamburgerStripe.classList.toggle('stripe-active');
+    timeout = setTimeout(navHideDelay, 200);
 })
 
 linkTea.addEventListener('click', e => {
-    navToggle.classList.toggle('nav-hide');
-    hamburgerStripe.classList.toggle('stripe-active');
+    timeout = setTimeout(navHideDelay, 200);
 })
 
 linkContact.addEventListener('click', e => {
+    timeout = setTimeout(navHideDelay, 200);
+})
+
+function navHideDelay() {
     navToggle.classList.toggle('nav-hide');
     hamburgerStripe.classList.toggle('stripe-active');
-})
+}
+
+// window.onscroll = function () {
+//     let isScrolled = false;
+//     const navBar = document.querySelector('#navbar')
+//     const navBarPoint = 600;
+
+//     function onScroll () {
+//         if (window.scrollY >= navBarPoint && !isScrolled) {
+//             console.log('peach')
+//             navBar.style.backgroundColor = 'var(--key-color';
+//             isScrolled = true;
+//         } else if (window.scrollY <= navBarPoint && !isScrolled) {
+//             navBar.style.backgroundColor = 'transparent';
+//             console.log('banana');
+//             isScrolled = false;
+//         } 
+//     }
+
+//     onScroll();
+//     return onScroll;
+// }
+
+
+
+/*ALT version scroll*/
+
+// const navBar = document.querySelector('#navbar')
+// const navBarPoint = 600;
+
+// window.addEventListener('scroll', e => {
+//     if (window.scrollY >= navBarPoint ) {
+//         console.log('peach')
+//         navBar.style.backgroundColor = 'var(--key-color';
+//     } else {
+//         navBar.style.backgroundColor = 'transparent';
+//         console.log('banana');
+//     }    
+// })
+
+/*Change?*/
+
+
+
+/*ALT save*/
+
+// linkGarden.addEventListener('click', e => {
+//     navToggle.classList.toggle('nav-hide');
+//     hamburgerStripe.classList.toggle('stripe-active');
+// })
+
+// linkTea.addEventListener('click', e => {
+//     navToggle.classList.toggle('nav-hide');
+//     hamburgerStripe.classList.toggle('stripe-active');
+// })
+
+// linkHome.addEventListener('click', e => {
+//     navToggle.classList.toggle('nav-hide');
+//     hamburgerStripe.classList.toggle('stripe-active');
+// })
+
+// linkContact.addEventListener('click', e => {
+//     navToggle.classList.toggle('nav-hide');
+//     hamburgerStripe.classList.toggle('stripe-active');
+// })
